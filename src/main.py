2b5019ab51215e146d05dab191b371f6d099550c
Ram Pun <rampun@Rams-MacBook-Pro.local>
2023-02-12 15:16:27 +0800

    initial commit

--- src/main.py
+++ src/main.py
@@ -3,10 +3,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+user_db = {
+    'jack': {'username': 'jack', 'date_joined': '2021-12-01', 'location': 'TST'},
+    'jill': {'username': 'jill', 'date_joined': '2021-12-02', 'location': 'YMT'},
+    'yueng': {'username': 'yueng', 'date_joined': '2021-12-03', 'location': 'SSP'},
+}
+
 
 @app.get('/')
 def get_hello_world_text():
-    return "Hello World!"
+    return {"result": "This is hello world text new updated"}
+
+
+@app.get('/users')
+def get_detail():
+    user_list = list(user_db)
+    return user_list
 
 
 if __name__ == "__main__":
